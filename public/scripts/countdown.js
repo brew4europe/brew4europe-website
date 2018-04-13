314e244ebf3b8279faf42973779a2701709e1537
James Taylor <jt-git@nti.me.uk>
2018-04-13 21:09:20 +0100

    Fix countdown to work on Sundays
    Signed-off-by: James Taylor <jt-git@nti.me.uk>

--- public/scripts/countdown.js
+++ public/scripts/countdown.js
@@ -1,8 +1,18 @@
 if (moment) {
     var now = moment();
-    var nextBrew = moment().weekday(7).hour(16).minute(0).second(0);
-    var nextBrewText = 'Next brew ' + now.to(nextBrew);
 
+    // Brew for Europe is currently 4pm every Sunday
+    var nextBrew = moment(now);
+    if((now.day() === 0) && now.hour() < 16) {
+        // Not long to wait, the next brew is this Sunday!
+        nextBrew.weekday(0);
+    } else {
+        // Have to wait until next Sunday now :(
+        nextBrew.weekday(7);
+    }
+    nextBrew.hour(16).minute(0).second(0);
+
+    var nextBrewText = 'Next brew ' + now.to(nextBrew);
     var countdown = document.getElementById('countdown');
     countdown.innerText = nextBrewText;
 }
